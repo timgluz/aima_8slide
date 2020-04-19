2171bd34f0ee9dbb503cad9205cd6d0da0133dd9
Timo Sulg <timgluz@gmail.com>
2020-04-19 14:53:50 +0200

    move debug print of searchnode into own fn

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ mod search;
 use frontiers::{Frontier, StackFrontier};
 use search::{SearchNode, SearchProblem};
 
+fn debug_search_node(current_node: &SearchNode) {
+    println!(
+        "step.{:?} {:?} - {:?}",
+        current_node.depth(),
+        current_node.action(),
+        current_node.item()
+    );
+}
+
 pub fn depth_first_tree_search(problem: Box<dyn SearchProblem>) -> Option<SearchNode> {
     let root_node = SearchNode::root(problem);
     let mut frontier = StackFrontier::new();
@@ -16,12 +25,7 @@ pub fn depth_first_tree_search(problem: Box<dyn SearchProblem>) -> Option<Search
         match frontier.remove() {
             None => break,
             Some(current_node) => {
-                println!(
-                    "step.{:?} {:?} - {:?}",
-                    current_node.depth(),
-                    current_node.action(),
-                    current_node.item()
-                );
+                //debug_search_node(&current_node);
 
                 if current_node.item().test_goal() {
                     return Some(current_node.clone());
