968b9f1a8d8ffb2ce4634ed41c174d81c7ea4441
Timo Sulg <timgluz@gmail.com>
2020-04-25 19:07:49 +0200

    refactor switching solver more dynamic

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ mod search;
 
 use search::uniform::*;
 
-fn solve_eight_puzzle(test_row: [u8; 9]) {
+enum SearchAlgorithm {
+    DepthFirst,
+    BreadthFirst,
+    UniformCost,
+}
+
+fn solve_eight_puzzle(test_row: [u8; 9], algorithm: SearchAlgorithm) {
     let initial_state = eight_puzzle::EightPuzzleState::new(test_row);
     if !initial_state.is_solveable() {
         println!("Unsolvable problem: {:?}", initial_state.value());
@@ -12,11 +18,13 @@ fn solve_eight_puzzle(test_row: [u8; 9]) {
     }
 
     let puzzle = Box::new(eight_puzzle::EightPuzzle::new(initial_state));
-    let solver = |puzzle| depth_first_search(puzzle);
-    let solver2 = |puzzle| breadth_first_search(puzzle);
-    let solver3 = |puzzle| uniform_cost_search(puzzle);
+    let maybe_solution = match algorithm {
+        SearchAlgorithm::DepthFirst => depth_first_search(puzzle),
+        SearchAlgorithm::BreadthFirst => breadth_first_search(puzzle),
+        SearchAlgorithm::UniformCost => uniform_cost_search(puzzle),
+    };
 
-    match solver3(puzzle) {
+    match maybe_solution {
         None => println!("no solution for {:?}", test_row),
         Some(node) => println!(
             "Found solution after {:?} steps: {:?}",
@@ -31,5 +39,5 @@ fn solve_eight_puzzle(test_row: [u8; 9]) {
 fn main() {
     let test_row = [1, 2, 3, 4, 0, 5, 7, 8, 6]; // 2-steps
                                                 //let test_row = [1, 2, 3, 7, 4, 5, 0, 8, 6];
-    solve_eight_puzzle(test_row);
+    solve_eight_puzzle(test_row, SearchAlgorithm::UniformCost);
 }
